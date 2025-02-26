d2e4989d9e88d910b0003da1da0a812b858004d1
SAGAR SATYARTH <sagarsatyarthh@gmail.com>
2025-02-26 23:02:34 +0530

    not solved

---
+++ PATTERNS/print_nCrInRightAngTriangle.java
@@ -0,0 +1,15 @@
+/*      1   
+        1   1
+        1   2   1
+        1   3   3   1
+        1   4   6   4   1
+        1   5   10  10  5   1
+ */
+import java.util.*;
+public class print_nCrInRightAngTriangle {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+
+    }
+    
+}
