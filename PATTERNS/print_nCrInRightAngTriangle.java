99a3fc9e23c434de284ac9521000b8240a19544c
SAGARSATYARTH <166894852+SAGARSATYARTH@users.noreply.github.com>
2025-02-27 22:41:17 +0530

    solved

--- PATTERNS/print_nCrInRightAngTriangle.java
+++ PATTERNS/print_nCrInRightAngTriangle.java
@@ -9,6 +9,18 @@ import java.util.*;
 public class print_nCrInRightAngTriangle {
     public static void main(String[] args) {
         Scanner scn=new Scanner(System.in);
+        System.out.print("Enter n : ");
+        int n=scn.nextInt();
+        for(int i=0;i<n;i++){
+            int icj=1;
+            for(int j=0;j<=i;j++){
+                System.out.print(icj+"\t");
+                int icjp1=icj*(i-j)/(j+1);
+                icj=icjp1;
+
+            }
+            System.out.println();
+        }
 
     }
     
